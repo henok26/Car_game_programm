7f4ec467f4c62acd73499241c3186c3790761c6d
henok26 <henokaniguse1931@gmail.com>
2022-10-01 01:22:45 +0300

    functionnn

---
+++ dict.py
@@ -0,0 +1,15 @@
+dict={"name":"henok",
+      "email":"henkniguse19@gmail.com",
+      "password":"141931h@H"
+      }
+
+students={
+    "henok":"g+1",
+    "dani":"villa",
+    "ybas":"lshape"}
+
+print(students["dani"])
+print(students["ybas"])
+print (students["henok"])
+for stud in students:
+    print(stud,students[stud] )
